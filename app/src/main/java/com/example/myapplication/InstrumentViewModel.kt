f775b08150e8dc405da982f080795532a367ef9b
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2024-06-11 09:26:55 -0400

    Revert "Update MainActivity.kt"
    This reverts commit 04b3cbf5306aa298a6487fd50aee07417c9dd168.

--- app/src/main/java/com/example/myapplication/InstrumentViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication
-
-import androidx.lifecycle.ViewModel
-
-class InstrumentViewModel: ViewModel() {
-    // initialize private instrument variables as empty list, mutable state flow
-    // create update instruments methods
-    // create filter instruments methods
-}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,41 +3,44 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
-    // get view model private variable -- ask chatgpt how to describe thi sin interview
-    private val viewModel: InstrumentViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    InstrumentScreen(viewModel) // pass in viewModel
+                    Greeting("Android")
                 }
             }
         }
     }
-    // create override for onResume, super.onResume and call fetch Instruments
-    // implement fetch instruments using a try catch inside a lifecycle coroutine
-    // in the try build the retrofit variable, and the api and response to getinstrumentdata
-    // catch the (e: Exception) and e.printStackTrace()
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!", modifier = modifier
+        text = "Hello $name!",
+        modifier = modifier
     )
 }
 
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
