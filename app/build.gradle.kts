4a1e4e92c77049129caa79da0816d3cac16c30f2
LuckyHariharan <lharihar@uwaterloo.ca>
2024-11-05 13:53:40 -0500

    add gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation ("androidx.compose.runtime:runtime-livedata:1.1.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
+    implementation ("com.ezylang:EvalEx:3.4.0")
 
     // Retrofit for network calls
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
