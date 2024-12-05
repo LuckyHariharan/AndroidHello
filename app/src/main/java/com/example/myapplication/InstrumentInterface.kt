eb3641f8157e3068c1f325a550c2f2ec20c5fcda
LuckyHariharan <lharihar@uwaterloo.ca>
2024-12-05 11:48:39 -0500

    Revert "setup picsum app"
    This reverts commit 4c2bfb2a21833824a82487967a068b6930ff3b54.

--- app/src/main/java/com/example/myapplication/InstrumentInterface.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication
-
-import retrofit2.Response
-import retrofit2.http.GET
-
-interface InstrumentInterface {
-    @GET("instruments.json")
-    suspend fun getInstruments(): Response<List<Instrument>>
-}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,32 +3,44 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Initialize Retrofit
-        val apiService = RetrofitInstance.api
-        // Create ViewModel with the API service
-        val viewModel = MainViewModel(apiService)
-
-        // Set the content to the MainScreen composable
         setContent {
-<<<<<<< Updated upstream
-            MainScreen(viewModel)
-=======
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    InstrumentScreen(viewModel = InstrumentViewModel())
+                    Greeting("Android")
                 }
             }
->>>>>>> Stashed changes
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
     }
 }
