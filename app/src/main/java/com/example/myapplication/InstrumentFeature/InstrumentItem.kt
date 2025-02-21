18ac3a413f0b85b2e0301e8f48b5154a76344d85
LuckyHariharan <lharihar@uwaterloo.ca>
2025-02-21 15:20:02 -0500

    organize projects
    both pokemon project and financial insturment package TODO

--- app/src/main/java/com/example/myapplication/InstrumentFeature/InstrumentItem.kt
+++ app/src/main/java/com/example/myapplication/InstrumentFeature/InstrumentItem.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.myapplication.InstrumentFeature
 
 data class InstrumentItem(
     val current_price: Double,
--- app/src/main/java/com/example/myapplication/InstrumentFeature/InstrumentViewModel.kt
+++ app/src/main/java/com/example/myapplication/InstrumentFeature/InstrumentViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.myapplication.InstrumentFeature
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
